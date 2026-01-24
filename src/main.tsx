746e168287a3ba995ac03adf76a292b464045742
Raione Machado <raionemachado20@gmail.com>
2026-01-23 21:59:46 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,30 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
+// META PIXEL (FORMA SEGURA PARA VITE)
+(function () {
+  const w = window as any;
+
+  if (w.fbq) return;
+
+  w.fbq = function () {
+    w.fbq.callMethod
+      ? w.fbq.callMethod.apply(w.fbq, arguments)
+      : w.fbq.queue.push(arguments);
+  };
+
+  w.fbq.queue = [];
+  w.fbq.loaded = true;
+  w.fbq.version = "2.0";
+
+  const script = document.createElement("script");
+  script.async = true;
+  script.src = "https://connect.facebook.net/en_US/fbevents.js";
+  document.head.appendChild(script);
+
+  w.fbq("init", "238045384187854");
+  w.fbq("track", "PageView");
+})();
+
+// RENDER APP
 createRoot(document.getElementById("root")!).render(<App />);
