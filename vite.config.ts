b0a2401e998782b48627c61da3cf09b7e2f71d24
Raione Machado <raionemachado20@gmail.com>
2026-01-23 22:30:06 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
-import { defineConfig } from 'vite'
+import { defineConfig, UserConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  plugins: [react()]
-})
+const config: UserConfig = {
+  plugins: [react()],
+}
+
+export default defineConfig(config)
