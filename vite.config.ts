f69f9ac1fcb968ddff46d9a5bfb51f6e392c2002
Raione Machado <raionemachado20@gmail.com>
2026-01-23 22:47:58 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-    },
-  },
-  build: {
-    outDir: 'dist',
-    sourcemap: false,
-  },
-  server: {
-    port: 3000,
-    open: true,
-  },
 })
